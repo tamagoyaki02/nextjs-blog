c78facd42c0584b3a053ffb9d3d8a30e646fd982
tamagoyaki02 <tamagoyaki0215@gmail.com>
2021-03-15 23:10:11 +0000

    first commit

---
+++ pages/posts/[id].js
@@ -0,0 +1,40 @@
+import Date from '../../components/date'
+import utilStyles from '../../styles/utils.module.css'
+import Head from 'next/head'
+import Layout from '../../components/layout'
+import { getAllPostIds, getPostData } from '../../lib/posts'
+
+//結果getPostDataで取得したpostDataオブジェクトがここに引数として入る
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
+    </Layout>
+  )
+}
+//id.jsではどんなページを生成する必要があるのかを判断してる -> .mdファイルからidを取得
+export async function getStaticPaths() {
+  const paths = getAllPostIds()
+  return {
+    paths,
+    fallback: false
+  }
+}
+//ビルド時に、コンテンツ内容をidに基づいて、getStaticPropsでデータをとってくる。必ずオブジェクト型で、keyにpropsを絶対持ってるルール
+export async function getStaticProps({ params }) {
+  const postData = await getPostData(params.id)
+  return {
+    props: {
+      postData
+    }
+  }
+}
